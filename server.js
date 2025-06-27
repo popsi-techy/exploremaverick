b22f4d5da34db4ad749627a1b85ea0385f6c78f8
popsi-techy <amanlearn13@gmail.com>
2025-06-27 09:17:51 +0530

    server update

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/blogs', express.static(path.join(__dirname, 'blogs')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/public/blogs', express.static(path.join(__dirname, 'blogs')));
+app.use('/public/images', express.static(path.join(__dirname, 'images')));
 
 
 ////
